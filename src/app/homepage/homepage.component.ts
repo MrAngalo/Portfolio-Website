fdccefc6b21cc8dee7cd383e2c70ae14abf7fffd
MrAngalo <guilherme.carvalho7@hotmail.com>
2024-12-31 17:17:36 -0300

    added icons to homepage

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,10 +1,15 @@
 import {Component} from '@angular/core';
 
-export type CardInfo = {
+export type ProjectCard = {
     image: string;
     title: string;
     description: string;
     redirect: string;
+};
+
+export type ProgramIcon = {
+    name: string;
+    image: string;
 };
 
 @Component({
@@ -15,8 +20,7 @@ export type CardInfo = {
     styleUrl: './homepage.component.scss'
 })
 export class HomepageComponent {
-    test = '/assets/300x350.svg';
-    projectCards: CardInfo[] = [
+    projectCards: ProjectCard[] = [
         {
             image: '/assets/300x350.svg',
             title: 'Project name #1',
@@ -28,6 +32,56 @@ export class HomepageComponent {
             title: 'Project name #1',
             description: 'Project description',
             redirect: '#'
+        }
+    ];
+    programIcons: ProgramIcon[] = [
+        {
+            name: 'Adobe Animate',
+            image: '/assets/content/homepage/program-icons/adobe-animate.png'
+        },
+        {
+            name: 'Audacity',
+            image: '/assets/content/homepage/program-icons/audacity.png'
+        },
+        {
+            name: 'Blender',
+            image: '/assets/content/homepage/program-icons/blender.png'
+        },
+        {
+            name: 'Character Creator',
+            image: '/assets/content/homepage/program-icons/character-creator.png'
+        },
+        {
+            name: 'Gimp',
+            image: '/assets/content/homepage/program-icons/gimp.png'
+        },
+        {
+            name: 'IClone',
+            image: '/assets/content/homepage/program-icons/iclone.png'
+        },
+        {
+            name: 'Maya',
+            image: '/assets/content/homepage/program-icons/maya.png'
+        },
+        {
+            name: 'Motive',
+            image: '/assets/content/homepage/program-icons/motive.png'
+        },
+        {
+            name: 'Photoshop',
+            image: '/assets/content/homepage/program-icons/photoshop.png'
+        },
+        {
+            name: 'Substance painter',
+            image: '/assets/content/homepage/program-icons/substance painter.png'
+        },
+        {
+            name: 'Unity',
+            image: '/assets/content/homepage/program-icons/unity.png'
+        },
+        {
+            name: 'Unreal',
+            image: '/assets/content/homepage/program-icons/unreal.png'
         }
     ];
 }
