d6d64e28e090b5eeebe23180aba2b6368a8b12c3
MrAngalo <guilherme.carvalho7@hotmail.com>
2024-12-31 12:29:50 -0300

    added card info

--- src/app/homepage/homepage.component.ts
+++ src/app/homepage/homepage.component.ts
@@ -1,5 +1,12 @@
 import {Component} from '@angular/core';
 
+export type CardInfo = {
+    image: string;
+    title: string;
+    description: string;
+    redirect: string;
+};
+
 @Component({
     selector: 'app-homepage',
     standalone: true,
@@ -9,4 +16,10 @@ import {Component} from '@angular/core';
 })
 export class HomepageComponent {
     test = '/assets/300x350.svg';
+    projectCards: CardInfo[] = Array(10).fill({
+        image: '/assets/300x350.svg',
+        title: 'Project name #1',
+        description: 'Project description',
+        redirect: '#'
+    });
 }
